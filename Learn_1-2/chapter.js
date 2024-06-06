b582396effb84b614734723967a590df80d25071
Dmitriy <1980sd80@mail.ru>
2024-06-07 00:00:20 +0400

    chapter 2.5

---
+++ Learn_1-2/chapter.js
@@ -0,0 +1,26 @@
+// 'use strict'
+// alert('I love JavaScript');
+
+// alert('Hello');
+
+// [1, 2].forEach(alert);
+
+let admin ;
+
+let name_admin = 'Jon';
+
+
+
+admin = name_admin;
+num = 5; 
+console.log("num:", num)
+
+console.log('admin:',admin);
+console.log('name:',name_admin);
+
+console.log(':',`${1 + 2}`);
+console.log(':',`${name_admin}`);
+
+console.log('typeof:',typeof (null));
+console.log('typeof:',typeof 1);
+
