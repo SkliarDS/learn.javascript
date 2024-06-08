75b6bf057273b381f95fa609a7d155eaaa623799
Dmitriy <1980sd80@mail.ru>
2024-06-08 15:59:31 +0400

    2 - 2.9

--- Learn_1-2/chapter.js
+++ Learn_1-2/chapter.js
@@ -24,3 +24,27 @@ console.log(':',`${name_admin}`);
 console.log('typeof:',typeof (null));
 console.log('typeof:',typeof 1);
 
+
+// let promt_name = prompt('Сколько тебе лет');
+// document.querySelector('#title').textContent = promt_name;
+
+// console.log('age:',age);
+// let isBoss = confirm("Ты здесь главный?");
+// console.log(' isBoss :', isBoss );
+
+let value = true;
+value = String(value); 
+console.log(typeof value); 
+console.log( "6" / "2" ); // 3, строки преобразуются в числа
+console.log(':',Number(null));
+console.log('undefined:',Number(undefined));
+
+let a = 1, b = 1;
+
+let c = ++a; // ?
+let d = b++; // ?
+
+console.log('c:',c); // 2
+console.log('a:',a); // 2
+console.log('b:',b); // 2
+console.log('d:',d); // 1
