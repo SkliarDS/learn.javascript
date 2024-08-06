192e6fb3df4ebcb9a6f655d62ebc53dd76937af2
Dmitriy <1980sd80@mail.ru>
2024-08-06 09:25:28 +0400

    chang files

--- 5/capter.js
+++ 5/capter.js
@@ -1,184 +1,36 @@
+let arr = ['i', 'go', 'home', 'we'];
+let numbers = [1, 4, 2, 10, 9];
+let newArr =  arr.slice(0, 1);
 
-console.log('Math:',Math.round(3.5));
-function random(min, max){
-    
-    return  Math.random() * (max - min);
-}
-console.log('random:',random(1, 5));
+console.log("newArr:", newArr)
 
-function random2(min, max){
-    
-    return  Math.floor( Math.random() * (max - min));
-}
-console.log('random:',random2(1, 5));
-
-let guestList = `Guests:
-  John
- * Pete
- * Mary
-`;
-console.log('guestList:',guestList);
-
-let str = 'Hello';
-let strNew = new String();
-console.log('str:',str.at(-3));
-console.log("strNew:", typeof strNew)
-
-for(let key of str){
-    console.log('key:',key);
-}
-
-let str1 = 'Widget with id'.toLowerCase();
-let str2 = 'WidgeT with id';
-let substr = 'wi';
-let pos = 0;
-let count = 0;
-for(let key of str1){
-    let foundPos = str1.indexOf(substr, pos);
-
-    if(foundPos == -1) break;
-    console.log('foundPos:',foundPos);
-    pos = foundPos + 1;
-    ++count;
-    
-    
-}
-console.log('count:',count);
-console.log('str2:', str2.includes(substr))
-
-let str3 = '';
-
-for (let i = 65; i <= 220; i++) {
-    str3 = str3 + String.fromCodePoint(i);
-}
-console.log('str3:',str3);
-
-let number = Number('1' * '5');
-console.log("number:", number)
-
-console.log('fff:', +(0.1  + 0.2).toFixed(3) === 0.3);
-
-let newStr = 'stringify'
-console.log('clice:',newStr.slice(0, 5));
-
-
-
-
-
-
-const ucFirst = function(str){
-    str = str.trim();
-    let firstLetter = str.at(0).toUpperCase();
-    let restLetter = str.slice(1);
-    return `${firstLetter}${restLetter}`;
+let arrayLike = {
+    0: "что-то",
+    1: "ещё",
+    2: "ещё",
+    [Symbol.isConcatSpreadable]: true,
+    length: 3,
 };
-console.log('ucFirst():',ucFirst("  вася ") == "Вася");
+arr.concat(arrayLike);
+console.log('arr:',arr.sort((a, b) => a > b ? 1 : -1));
+console.log('numbers:',numbers.sort((a, b) => a - b));
 
-const checkSpam = function(str) {
-    str = str.trim().toLowerCase();
-    return (str.includes('XXX'.toLowerCase())  || str.includes('ViAgRA'.toLowerCase())) ? true : false ;
-}
-console.log('checkSpam:',checkSpam('buy ViAgRA now'));
-console.log('checkSpam2:',checkSpam('innocent rabbit'));
-console.log('checkSpam3:',checkSpam('free xXxxx'));
+let users = [
+    {id: 1, name: "Вася"},
+    {id: 2, name: "Петя"},
+    {id: 3, name: "Маша"},
+    {id: 4, name: "Петя"},
+];
 
-function truncate(str, maxlength){
-    str = str.trim();
-    if(str.length >= maxlength){
-        return str = `${str.substr(0, maxlength - 1)}...` 
-    } else {
-        return str;
-    }
-};
-
-console.log('truncate:',truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
-console.log('truncate1:',truncate("Всем привет!", 20));
-
-function  extractCurrencyValue(str){
-    return +str.slice(1);
-}
-console.log('extractCurrncyValur:',extractCurrencyValue('$120') === 120 );
-
-function DNAtoRNA(dna) {
-    let newStr = '';
-    for(let key of dna){        
-        if(key == 'T'){
-            newStr += 'U';
-        } else {
-            newStr += key;
-        }
-    }
-    return newStr;
-}
-console.log('DNAtoRNA(dna):',DNAtoRNA('TTRT'));
-console.log('DNAtoRNA(dna):',DNAtoRNA('GCAT'));
-console.log('DNAtoRNA(dna):',DNAtoRNA('GACCGCCGCC'));
-
-
-let styles = ["Джаз", "Блюз",];
-styles.push('Рок-н-ролл');
-let middle = Math.floor(styles.length / 2); 
-styles[middle] = 'Классика';
-let firstItem = styles.shift();
-styles.unshift('Rap', 'Reggi')
-console.log("firstItem:", firstItem)
-console.log('styles:',styles);
-
-
-let arr = ["a", "b", function(){console.log('this:',this)},];
-
-arr.push(function() {
-    console.log( this );
+let user = users.map((item, index, arr )=> {
+    return item;
 });
 
-arr[2]() // ?
-arr[3]() // ?
+console.log('user:',user);
 
-function sumInput(){
-    let arr_num = [];
-    let num;
-    while(true){
-        num = prompt('?');
-        if(num == null || !Number.isNaN(num) || num == '')break;
-        arr_num.push(parseInt(num));   
-    }
-    let sum = 0;
-    for(let number of arr_num){
-        sum += number;
-    }
-    return sum;   
-}
-// console.log('sumInput():',sumInput());
+let names = 'Вася,Петя,Маша';
+let str = "тест";
+let arrNames = names.split(',');
+console.log('str:',arrNames.join('/'));
+console.log("arrNames:", arrNames)
 
-const readNumber = () => {
-    let num;
-    while(true){
-        num = +prompt('?');
-        if(!Number.isNaN(num))break;
-    }
-    if(num == null || num == ''){
-        num = 'null';
-    }
-    return num;
-};
-// console.log('readNumber:',readNumber());
-
-// const num1 = parseInt(prompt('?', ));
-// const num2 = parseInt(prompt('?', ));
-
-// if(!Number.isNaN(num1) && !Number.isNaN(num2)){
-//     console.log('sum:', num1 + num2);
-// } else {
-//     alert('Введите число')
-// }
-
-let fruits = ["Яблоко", "Апельсин", "Груша"];
-fruits[fruits.length] = 'limon';
-fruits.age = '25'
-fruits.age = ''
-fruits.pop();
-delete fruits.age;
-console.log('fruits:', fruits);
-for(let key of fruits){
-    console.log('key:', key);
-}
--- tasks-array.js
+++ tasks-array.js
@@ -1,5 +1,188 @@
+function differenceInAges(ages){
+    let max = -Infinity;
+    let min = ages[0];
+    for(let i = 0; i < ages.length; i++){
+		if(ages[i] > max){
+			max = ages[i];
+		} 
+		if(ages[i] < min){
+			min = ages[i];
+		}
+    }    
+    return [max, max - min, min]
+}
+
+console.log('differenceInAges(ages):', differenceInAges([82, 15, 6, 38, 35]));
+
+function camelize(str) {
+	let arr = str.split('-');
+	let newArr = arr.map((item, index )=> {
+		let newStr = '';
+		if(index !== 0){
+			newStr = `${item[0].toUpperCase()}${item.slice(1)}`;
+		} else {
+			newStr = item;
+		}
+		return newStr;
+	})
+	str = newArr.join('');
+	return str;
+};
+console.log('camelize(str):', camelize("background-color"));
+console.log('camelize(str):', camelize("-webkit-transition"));
+
+function filterRange(arr, a, b) {
+	let newArr = arr.filter(item => {
+		if(a <= item && item <= b ){
+			return item;
+		}
+	});
+	return newArr;
+}
+console.log('filterRange():',filterRange([5, 3, 8, 1, 2, 4], 1, 5));
+
+function filterRangeInPlace(arr, a, b) {
+	arr.forEach((item, index) => {	
+		if(item < a || item > b) {
+			arr.splice(index , 1);
+			index--;
+		};
+	})
+}
+let arr = [5, 3, 8, 1];
+filterRangeInPlace(arr, 1, 4)
+console.log('filterRangeInPlace():',arr);
 
 
+let arr2 = [5, 2, 1, -10, 8]
+arr2.sort((a, b) => b - a)
+
+console.log('arr2:',arr2);
+
+let arrStr = ["HTML", "JavaScript", "CSS"];
+function copySorted(arr) {
+	let arrNew = [];
+	for(let key of arr){
+		arrNew.push(key);
+	}
+	return arrNew.sort();
+}
+
+let sorted = copySorted(arrStr);
+console.log('sorted:',sorted);
+console.log('arrStr:',arrStr);
+
+
+function Calculator(){
+	
+	this.method = {
+		'+': (a, b) => a + b,
+		'-': (a, b) => a - b,
+	}
+	this.calculate = function(str){
+	 	let arr = str.split(' ');
+		let a = +arr[0];
+		let operator = arr[1];
+		let b = +arr[2];
+
+		return this.method[operator](a, b);
+	}
+	this.addMethod = function(name, func){
+		this.method[name] = func;		
+	}
+}
+
+let calc = new Calculator;
+console.log("calc:", calc.calculate('2 + 5'))
+console.log("calc:", calc.calculate('2 - 5'))
+
+let powerCalc = new Calculator;
+powerCalc.addMethod("**", (a, b) => a ** b)
+let result = powerCalc.calculate('2 ** 5');
+console.log("result:", result)
+
+
+let vasya = { name: "Вася", surname: "Пупкин", id: 1, age: 20};
+let petya = { name: "Петя", surname: "Иванов", id: 2, age: 40};
+let masha = { name: "Маша", surname: "Петрова", id: 3, age: 30};
+
+let users = [ vasya, petya, masha ];
+
+let names = users.map(item => item.name)
+
+console.log('names:',names);
+
+let usersMapped = users.map(item => {
+	return {
+		fullName: `${item.name} ${item.surname}`,
+		id: item.id
+	}
+});
+console.log('usersMapped:',usersMapped);
+
+function sortByAge(arr){
+	arr.sort((a, b) => a.age - b.age);
+	return arr;
+};
+console.log('sortByAge(arr):',sortByAge(users));
+
+let arrNumbers = [0, 4, 3, 7, 2];
+function shuffle(array){
+	let a = array.shift();
+	array.push(a);
+	return array;
+}
+console.log('shuffle(array):',shuffle(arrNumbers));
+console.log('shuffle(array):',shuffle(arrNumbers));
+
+function getAverageAge(users){
+	users.reduce((acc, item) => {
+		return sum = acc + item.age;
+	}, 0)
+	return Math.floor(sum / (users.length));
+}
+console.log('getAverageAge(users):',getAverageAge(users));
+
+let arrUsers = [
+	{id: 'john', name: "John Smith", age: 20},
+	{id: 'ann', name: "Ann Smith", age: 24},
+	{id: 'pete', name: "Pete Peterson", age: 31},
+];
+function groupById(users){
+	// let newArr = {};
+	// for(let obj of users){
+	// 	newArr[obj.id] = obj;	
+	// }
+	// return newArr;
+	return users.reduce((obj, item) => {
+		obj[item.id] = item;
+		return obj;
+	}, {});
+};
+let usersById = groupById(arrUsers);
+console.log('usersById:',usersById);
+
+function unique(arr) {
+	let newArr = [];
+	for(let key of arr){
+		if(!newArr.includes(key)){
+			newArr.push(key);
+		}
+	}
+	return newArr;
+}
+
+let strings = ["кришна", "кришна", "харе", "харе",
+	"харе", "харе", "кришна", "кришна", ":-O"
+];
+console.log('unique(arr):',unique(strings));
+
+let range = {
+	from: 1,
+	to: 5
+  };
+  let arr4 = Array.from(range);
+  console.log('arr4:',arr4);
 // =======================================================================
 // 1
 // ==========================================================================
@@ -43,7 +226,7 @@ console.log(findVowels('generator'))// --> 4
 // buzz
 
 function fizzBuzz(n) {
-  	
+
 }
 
 console.log(fizzBuzz(5))
@@ -55,12 +238,17 @@ console.log(fizzBuzz(5))
 // * Ожидаемый результат: ({ a: 1, b: 2 }, { c: 1, b: 2 }) => { b: 2 }
 
 const intersection = (firstObj, secondObj) => {
-  //code here ...
-
+  let obj = {};
+  for(let key in firstObj){
+	if(key in secondObj && firstObj[key] == secondObj[key]){
+		obj[key] = firstObj[key];
+	}
+  }
+  return obj;
 }
 
-const data = {a: 1, b: 2};
-const data2 = {c: 1, b: 2};
+const data = {a: 1, b: 2, c: 1};
+const data2 = {c: 1, b: 2, g: 3};
 console.log(intersection(data, data2)); // { b: 2 }
 // =======================================================================
 //4
