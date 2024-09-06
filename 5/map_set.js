e2c3a9bec6fd44191b12cd1f31d4b73c1e267705
Dmitriy <1980sd80@mail.ru>
2024-09-06 10:21:16 +0400

    add tasks map_set.js

--- 5/map_set.js
+++ 5/map_set.js
@@ -93,9 +93,47 @@ console.log('getDaysInMonth(month):', getDaysInMonth(monthDays));
 /* 
 Напишите функцию, которая вернет новый массив, содержащий только уникальные числа из исходного массива.
 */ 
-const numbers = [1, 2, 2, 3, 4, 4, 5, 6, 7, 7, 8];
+const numbers = [3, 1, 2, 2, 3, 4, 4, 5, 6, 7, 7, 8];
 
 function uniqueNumbers(nums){
-    return new Set(nums);
+    return Array.from(new Set(nums));
 };
 console.log('uniqueNumbers(numbers):',uniqueNumbers(numbers));
+
+/* 
+Напишите функцию, которая принимает объект и возвращает его вес. 
+Если объект не найден, функция должна возвращать undefined.
+*/
+
+let person1 = { name: 'Alice' };
+let person2 = { name: 'Bob' };
+let person3 = { name: 'Charlie' };
+
+const weights = new WeakMap([
+    [person1, 60],
+    [person2, 75],
+    [person3, 85]
+]);
+person1 = null;
+function sizePerson(person){
+   return weights.get(person);
+   
+}
+console.log('sizePerson:',sizePerson(person1));
+
+/* Напишите функцию, которая добавляет объекты в WeakSet, если у них есть свойство active со значением true.*/ 
+let obj1 = { id: 1, active: true };
+let obj2 = { id: 2, active: false };
+let obj3 = { id: 3, active: true };
+let obj4 = { id: 4 };
+
+const activeObjects = new WeakSet();
+
+function addIfActive(obj){
+    for(let key of Object.values(obj)){
+        if(key == true){
+            return activeObjects.add(obj)  
+        }
+    }
+}
+console.log(' addIfActive(obj):', addIfActive(obj1), addIfActive(obj2));
